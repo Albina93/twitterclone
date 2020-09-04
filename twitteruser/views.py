465de8e2cf99ddb38e3fcbd8dca2575207557209
Albina1993 <alba.tleubergen@gmail.com>
2020-09-04 10:21:31 -0400

    added following, unfollowing users part

--- twitteruser/views.py
+++ twitteruser/views.py
@@ -6,24 +6,28 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url="loginpage")
 def index_view(request):
-    tweets = Tweet.objects.all().order_by('-created_time')
     total_tweets = Tweet.objects.filter(twitter_user__username=request.user.username).count()
+    
+    user_tweets = Tweet.objects.filter(twitter_user=request.user).order_by('-created_time') 
+    following_tweets = Tweet.objects.filter(twitter_user__in=request.user.following.all()).order_by('-created_time') 
+    tweets = user_tweets | following_tweets
+
     return render(request, 'index.html', {'tweets': tweets, 'total_tweets': total_tweets})
 
 
 
 def profile_view(request, user_name):
+    user_profile = TwitterUserModel.objects.get(username=user_name)
     tweets = Tweet.objects.filter(twitter_user__username=user_name).order_by('-created_time')
     total_tweets = tweets.count()
-    return render(request, 'index.html', {'tweets': tweets, 'total_tweets': total_tweets})
+    following_list = request.user.following.all()
+    return render(request, 'profile.html', {'tweets': tweets, 'total_tweets': total_tweets,  'user_profile': user_profile, 'following_list': following_list})
 
 
 def add_follow_view(request, user_name):
     user_follow = TwitterUserModel.objects.get(username=user_name)
     login_user = TwitterUserModel.objects.get(username=request.user.username)
     login_user.following.add(user_follow)
-    
-
     login_user.save()
 
 
